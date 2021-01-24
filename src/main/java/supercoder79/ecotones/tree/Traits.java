72e6be733586ecf16c51cabbedeaa036ca58ada9
SuperCoder7979 <25208576+SuperCoder7979@users.noreply.github.com>
2021-01-24 10:05:29 -0500

    slight texture and trait work

--- src/main/java/supercoder79/ecotones/tree/Traits.java
+++ src/main/java/supercoder79/ecotones/tree/Traits.java
@@ -13,7 +13,7 @@ import supercoder79.ecotones.tree.smallspruce.WideSmallSpruceTrait;
 import java.util.List;
 
 public class Traits {
-    public static final List<OakTrait> OAK = ImmutableList.of(new DefaultOakTrait(), new ThinOakTrait(), new WarpedOakTrait(), new SmallerOakTrait(), new SparseOakTrait());
+    public static final List<OakTrait> OAK = ImmutableList.of(new DefaultOakTrait(), new ThinOakTrait(), new WarpedOakTrait(), new SmallerOakTrait());
     public static final List<SmallSpruceTrait> SMALL_SPRUCE = ImmutableList.of(new DefaultSmallSpruceTrait(), new ShortSmallSpruceTrait(), new WideSmallSpruceTrait());
     public static final List<PoplarTrait> POPLAR = ImmutableList.of(new DefaultPoplarTrait(), new ShortPoplarTrait(), new WidePoplarTrait(), new TaperedPoplarTrait());
 
