f8977d1baa12e62d50279c2111b55caf0aaae3fe
SuperCoder79 <25208576+SuperCoder7979@users.noreply.github.com>
2021-04-06 09:46:43 -0400

    Add redstone functionality to geysers

--- src/main/java/supercoder79/ecotones/blocks/GeyserBlock.java
+++ src/main/java/supercoder79/ecotones/blocks/GeyserBlock.java
@@ -2,34 +2,74 @@ package supercoder79.ecotones.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
+import net.minecraft.world.World;
 import net.minecraft.world.explosion.Explosion;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 import java.util.Random;
 
 public class GeyserBlock extends Block {
+    public static final BooleanProperty TRIGGERED = Properties.TRIGGERED;
     public GeyserBlock(Block.Settings settings) {
         super(settings);
+        this.setDefaultState(this.getStateManager().getDefaultState().with(TRIGGERED, false));
     }
 
     @Override
     public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
         if (world.getBlockState(pos.up()).isOpaque()) {
             world.createExplosion(null, pos.getX(), pos.getY(), pos.getZ(), 6f, Explosion.DestructionType.DESTROY);
-            world.spawnParticles(ParticleTypes.POOF, pos.getX(), pos.getY() + 0.125, pos.getZ(), 200, random.nextDouble() * 0.005, 0.65, random.nextDouble() * 0.005, 0.75f);
+            world.spawnParticles(ParticleTypes.POOF, pos.getX() + 0.5, pos.getY() + 0.125, pos.getZ() + 0.5, 200, random.nextDouble() * 0.005, 0.65, random.nextDouble() * 0.005, 0.5f);
         }
 
-        world.spawnParticles(ParticleTypes.POOF, pos.getX(), pos.getY() + 0.125, pos.getZ(), 50, random.nextDouble() * 0.005, 1, random.nextDouble() * 0.005, 0.75f);
-        List<LivingEntity> list = world.getNonSpectatingEntities(LivingEntity.class, new Box(pos.add(-2, -2, -2), pos.add(2, 2, 2)));
+        world.spawnParticles(ParticleTypes.POOF, pos.getX() + 0.5, pos.getY() + 0.125, pos.getZ() + 0.5, 50, random.nextDouble() * 0.005, 1, random.nextDouble() * 0.005, 0.5f);
+        List<LivingEntity> list = world.getNonSpectatingEntities(LivingEntity.class, new Box(pos.add(-3, -2, -3), pos.add(3, 6, 3)));
         for (LivingEntity entity : list) {
-            entity.addVelocity(0, 1.25, 0);
+            entity.addVelocity(0, 1.5, 0);
             entity.damage(DamageSource.GENERIC, 4);
+        }
+    }
+
+    @Override
+    public void neighborUpdate(BlockState state, World world, BlockPos pos, Block block, BlockPos fromPos, boolean notify) {
+        super.neighborUpdate(state, world, pos, block, fromPos, notify);
+
+        boolean isPowered = world.isReceivingRedstonePower(pos) || world.isReceivingRedstonePower(pos.up());
+        boolean isTriggered = state.get(TRIGGERED);
+
+        if (isPowered && !isTriggered) {
+            world.getBlockTickScheduler().schedule(pos, this, 4);
+            world.setBlockState(pos, state.with(TRIGGERED, true), 3);
+        } else if (!isPowered && isTriggered) {
+            world.setBlockState(pos, state.with(TRIGGERED, false), 3);
+        }
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(TRIGGERED);
+    }
+
+    @Override
+    public void afterBreak(World world, PlayerEntity player, BlockPos pos, BlockState state, @Nullable BlockEntity blockEntity, ItemStack stack) {
+        super.afterBreak(world, player, pos, state, blockEntity, stack);
+
+        if (!world.isClient) {
+            Random random = world.getRandom();
+            ((ServerWorld)world).spawnParticles(ParticleTypes.POOF, pos.getX() + 0.5, pos.getY() + 0.125, pos.getZ() + 0.5, 50, random.nextDouble() * 0.005, 1, random.nextDouble() * 0.005, 0.5f);
         }
     }
 }
--- src/main/java/supercoder79/ecotones/world/biome/base/warm/CoolSteppeBiome.java
+++ src/main/java/supercoder79/ecotones/world/biome/base/warm/CoolSteppeBiome.java
@@ -129,6 +129,12 @@ public class CoolSteppeBiome extends EcotonesBiomeBuilder {
                         .spreadHorizontally()
                         .applyChance(8));
 
+        this.addFeature(GenerationStep.Feature.RAW_GENERATION,
+                EcotonesFeatures.ROCK_SPIRE.configure(FeatureConfigHolder.STONE_SPIRE)
+                        .decorate(Decorator.HEIGHTMAP.configure(NopeDecoratorConfig.INSTANCE))
+                        .spreadHorizontally()
+                        .applyChance(8));
+
         BiomeHelper.addDefaultSpawns(this.getSpawnSettings());
         BiomeHelper.addDefaultFeatures(this);
     }
