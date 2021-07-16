d6e121b1bb1a7c2a5e2387ad8d4a0f3f4bc5ac28
SuperCoder79 <25208576+SuperCoder7979@users.noreply.github.com>
2021-07-16 12:51:48 -0400

    Add sap distillery shift clicking

--- src/main/java/supercoder79/ecotones/screen/SapDistilleryScreenHandler.java
+++ src/main/java/supercoder79/ecotones/screen/SapDistilleryScreenHandler.java
@@ -4,10 +4,14 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.inventory.SimpleInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.recipe.BrewingRecipeRegistry;
 import net.minecraft.screen.ArrayPropertyDelegate;
 import net.minecraft.screen.PropertyDelegate;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.slot.Slot;
+import net.minecraft.tag.ItemTags;
+import supercoder79.ecotones.items.EcotonesItems;
 
 public class SapDistilleryScreenHandler extends ScreenHandler {
     private final PropertyDelegate propertyDelegate;
@@ -20,8 +24,8 @@ public class SapDistilleryScreenHandler extends ScreenHandler {
         super(EcotonesScreenHandlers.SAP_DISTILLERY, syncId);
         this.propertyDelegate = propertyDelegate;
 
-        this.addSlot(new Slot(inventory, 0, 26, 37 - 7));
-        this.addSlot(new Slot(inventory, 1, 80, 79 - 7));
+        this.addSlot(new SapSlot(inventory, 0, 26, 37 - 7));
+        this.addSlot(new LogSlot(inventory, 1, 80, 79 - 7));
 
         int k;
         for(k = 0; k < 3; ++k) {
@@ -53,10 +57,79 @@ public class SapDistilleryScreenHandler extends ScreenHandler {
         return this.propertyDelegate.get(3);
     }
 
-    // TODO: implement shift clicking
+    @Override
+    public ItemStack transferSlot(PlayerEntity player, int index) {
+        ItemStack itemStack = ItemStack.EMPTY;
+        Slot slot = this.slots.get(index);
+
+        if (slot != null && slot.hasStack()) {
+            ItemStack itemStack2 = slot.getStack();
+            itemStack = itemStack2.copy();
+
+            if (index != 0 && index != 1) {
+                if (itemStack.isIn(ItemTags.LOGS_THAT_BURN)) {
+                    if (this.insertItem(itemStack2, 1, 2, false)) {
+                        return ItemStack.EMPTY;
+                    }
+                } else if (itemStack.getItem() == EcotonesItems.MAPLE_SAP) {
+                    if (this.insertItem(itemStack2, 0, 1, false)) {
+                        return ItemStack.EMPTY;
+                    }
+                }
+            } else {
+                if (!this.insertItem(itemStack2, 3, 38, true)) {
+                    return ItemStack.EMPTY;
+                }
+
+                slot.onQuickTransfer(itemStack2, itemStack);
+            }
+
+            if (itemStack2.isEmpty()) {
+                slot.setStack(ItemStack.EMPTY);
+            } else {
+                slot.markDirty();
+            }
+
+            if (itemStack2.getCount() == itemStack.getCount()) {
+                return ItemStack.EMPTY;
+            }
+
+            slot.onTakeItem(player, itemStack2);
+        }
+
+        return itemStack;
+    }
 
     @Override
     public boolean canUse(PlayerEntity player) {
         return true;
+    }
+
+    static class SapSlot extends Slot {
+        public SapSlot(Inventory inventory, int i, int j, int k) {
+            super(inventory, i, j, k);
+        }
+
+        public boolean canInsert(ItemStack stack) {
+            return stack.getItem() == EcotonesItems.MAPLE_SAP;
+        }
+
+        public int getMaxItemCount() {
+            return 64;
+        }
+    }
+
+    static class LogSlot extends Slot {
+        public LogSlot(Inventory inventory, int i, int j, int k) {
+            super(inventory, i, j, k);
+        }
+
+        public boolean canInsert(ItemStack stack) {
+            return stack.isIn(ItemTags.LOGS_THAT_BURN);
+        }
+
+        public int getMaxItemCount() {
+            return 64;
+        }
     }
 }
