1bb6fbd3126c7c482e7a07102668cae6b5d67016
TehcJS <tehcdev@gmail.com>
2021-07-21 11:51:11 -0400

    Fix Formatting

--- src/main/java/supercoder79/ecotones/mixin/client/MixinBackgroundRenderer.java
+++ src/main/java/supercoder79/ecotones/mixin/client/MixinBackgroundRenderer.java
@@ -13,31 +13,14 @@ import supercoder79.ecotones.client.FogHandler;
 
 @Mixin(BackgroundRenderer.class)
 public class MixinBackgroundRenderer {
-//    @Redirect(method = "applyFog", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderFogStart(F)V", ordinal = 1))
-//    private static void applyEcotonesFancyFog(float start) {
-//        MinecraftClient client = MinecraftClient.getInstance();
-//        RenderSystem.setShaderFogStart(start);
-//
-//        if (ClientSidedServerData.isInEcotonesWorld) {
-//            float heightMultiplier = (float) (MathHelper.clamp(client.player.getY() - 124, 0, 8) / 8);
-//
-//            long time = client.world.getLunarTime();
-//
-//            // TODO: biome humidity
-//            RenderSystem.setShaderFogStart(Math.min(start, (float) (start * FogHandler.multiplierFor(time)) * heightMultiplier));
-//        } else {
-//            RenderSystem.setShaderFogStart(start);
-//        }
-//    }
-    
     @ModifyArg(method = "applyFog", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderFogStart(F)V", ordinal = 1))
     private static float applyEcotonesFancyFog(float start) {
         if (ClientSidedServerData.isInEcotonesWorld) {
             MinecraftClient client = MinecraftClient.getInstance();
-    
+            
             assert client.player != null;
             float heightMultiplier = (float) (MathHelper.clamp(client.player.getY() - 124, 0, 8) / 8);
-    
+            
             assert client.world != null;
             long time = client.world.getLunarTime();
             
