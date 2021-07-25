01819e237c205ae68707119bb32315ccce75d236
SuperCoder79 <25208576+SuperCoder7979@users.noreply.github.com>
2021-07-25 18:55:39 -0400

    Disable custom fog mixin

--- src/main/java/supercoder79/ecotones/mixin/client/MixinBackgroundRenderer.java
+++ src/main/java/supercoder79/ecotones/mixin/client/MixinBackgroundRenderer.java
@@ -12,20 +12,20 @@ import supercoder79.ecotones.client.FogHandler;
 
 @Mixin(BackgroundRenderer.class)
 public class MixinBackgroundRenderer {
-    @Redirect(method = "applyFog", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderFogStart(F)V", ordinal = 1))
-    private static void applyEcotonesFancyFog(float start) {
-        MinecraftClient client = MinecraftClient.getInstance();
-        RenderSystem.setShaderFogStart(start);
-
-//        if (ClientSidedServerData.isInEcotonesWorld) {
-//            float heightMultiplier = (float) (MathHelper.clamp(client.player.getY() - 124, 0, 8) / 8);
+//    @Redirect(method = "applyFog", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderFogStart(F)V", ordinal = 1))
+//    private static void applyEcotonesFancyFog(float start) {
+//        MinecraftClient client = MinecraftClient.getInstance();
+//        RenderSystem.setShaderFogStart(start);
 //
-//            long time = client.world.getLunarTime();
-//
-//            // TODO: biome humidity
-//            RenderSystem.setShaderFogStart(Math.min(start, (float) (start * FogHandler.multiplierFor(time)) * heightMultiplier));
-//        } else {
-//            RenderSystem.setShaderFogStart(start);
-//        }
-    }
+////        if (ClientSidedServerData.isInEcotonesWorld) {
+////            float heightMultiplier = (float) (MathHelper.clamp(client.player.getY() - 124, 0, 8) / 8);
+////
+////            long time = client.world.getLunarTime();
+////
+////            // TODO: biome humidity
+////            RenderSystem.setShaderFogStart(Math.min(start, (float) (start * FogHandler.multiplierFor(time)) * heightMultiplier));
+////        } else {
+////            RenderSystem.setShaderFogStart(start);
+////        }
+//    }
 }
