3c96b88e37ad7ae8686250d2c37958683dc195c5
Jasmine Karthikeyan <25208576+jaskarth@users.noreply.github.com>
2023-07-06 11:58:26 -0400

    Fix crash with QSL

--- src/main/java/supercoder79/ecotones/mixin/MixinBiomeSelectors.java
+++ src/main/java/supercoder79/ecotones/mixin/MixinBiomeSelectors.java
@@ -3,13 +3,16 @@ package supercoder79.ecotones.mixin;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectionContext;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(BiomeSelectors.class)
+@Mixin(targets = {"net.fabricmc.fabric.api.biome.v1.BiomeSelectors", "org.quiltmc.qsl.worldgen.biome.api.BiomeSelectors"})
+@Pseudo
 public class MixinBiomeSelectors {
-    @Inject(method = "lambda$foundInOverworld$3", at = @At("HEAD"), cancellable = true)
+    // Somehow this works, as both lambdas are 3rd in sequence in the classfile
+    @Inject(method = "lambda$foundInOverworld$3", at = @At("HEAD"), cancellable = true, require = 0)
     private static void selectEcotonesAsOverworld(BiomeSelectionContext context, CallbackInfoReturnable<Boolean> cir) {
         if (context.getBiomeKey().getValue().getNamespace().equals("ecotones")) {
             cir.setReturnValue(true);
